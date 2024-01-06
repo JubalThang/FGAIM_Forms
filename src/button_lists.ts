872f369ea964845b5ec28e58a3069cab8a5e5f36
JubalThang <jubalthang@gmail.com>
2024-01-06 12:49:33 -0600

    add submission button

--- src/button_lists.ts
+++ src/button_lists.ts
@@ -8,5 +8,9 @@ export const buttons : Button[] = [
     {
       "name": "Baby Dedication Form",
       "link": "https://form.jotform.com/233624310013136"
+    },
+    {
+      "name": "Submission",
+      "link": "https://www.jotform.com/tables/233384594517162?st=TTgwbHI4RGhiNGF4RTNZQXpWY1Z0VzdHclhVbW9CalFWVmRPRTA0SGFyOGhsdmc1TGNSN2htU0x4dkVyVVcxa203SEpUbHdQdkw0YkZJcjBoTjd5VmpEdnZoTmJZRWZhZ2VpWFBNQ2d6TmtPc282Y3ZrODZoZHVGNXRXbElKc28="
     }
   ]
